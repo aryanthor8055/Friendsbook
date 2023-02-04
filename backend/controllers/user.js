dc8716c4b62d79e44a10afff69a46eb89281358f
Aryan <aryan16072001236809@gmail.com>
2023-02-04 23:35:02 +0530

    Authentication(Backend)--Setup User Modals,Adding jwt,Google Oauth Playground and Complete verification email with node mailer

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,3 +1,117 @@
-exports.home = (req, res) => {
-    res.send("selcome from user home")
+const { validateEmail, validateLength, validateUsername } = require('../helpers/validation')
+const bcrypt = require('bcrypt')
+const User = require("../models/User");
+const { generateToken } = require('../helpers/tokens');
+const { jwt } = require('jsonwebtoken')
+exports.register = async (req, res) => {
+    try {
+        const { first_name, last_name, email, password, username, bYear, bMonth, bDay, gender } = req.body;
+
+        if (!validateEmail(email)) {
+            return res.status(400).json({
+                message: "invalid email address",
+            })
+        }
+
+        const check = await User.findOne({ email });
+        if (check) {
+            return res.status(400).json({
+                message: "The email address already exists,try wiith a different email address",
+            })
+        }
+        if (!validateLength(first_name, 3, 30)) {
+            return res.status(400).json({
+                message: "first name must between 3 and 30 characters.",
+            })
+        }
+        if (!validateLength(last_name, 3, 30)) {
+            return res.status(400).json({
+                message: "last name must between 3 and 30 characters.",
+            })
+        }
+        if (!validateLength(password, 6, 40)) {
+            return res.status(400).json({
+                message: "password must be atleast 6 characters.",
+            })
+        }
+        const cryptedPassword = await bcrypt.hash(password, 12);
+
+        let tempUsername = first_name + last_name;
+        let newUsername = await validateUsername(tempUsername)
+        const user = await new User({
+            first_name,
+            last_name,
+            email,
+            password: cryptedPassword,
+            username, newUsername,
+            bYear,
+            bMonth,
+            bDay,
+            gender
+        }).save();
+
+        const emailVerificationToken = generateToken({ id: user._id.toString() }, "30m");
+        const url = `${process.env.BASE_URL}/activate/${emailVerificationToken}`
+        sendVerificationEmail(user.email, user.first_name, url);
+        const token = generateToken({ id: user._id.toString() }, "7d")
+        res.send({
+            id: user._id,
+            username: user.username,
+            picture: user.picture,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            token: token,
+            verified: user.verified,
+            message: "Register Success ! please activate your email to start"
+        })
+        res.json(user)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+};
+exports.activateAccount = async (req, res) => {
+
+    try {
+        const { token } = req.body;
+        const user = jwt.verify(token, process.env.TOKEN_SECRET);
+        const check = await User.findById(user.id);
+        if (check.verified === true) {
+            return res.status(400).json({ message: "this email is already activated" })
+        } else {
+            await User.findByIdAndUpdate(user.id, { verified: true })
+            return res.status(200).json({ message: "Account has been activated Successfully" })
+        }
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+
+};
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: 'the email address you entered is not connected to an account' })
+        }
+        const check = await bcrypt.compare(password, user.password);
+        if (!check) {
+            return res.status(400).json({ message: 'Invalid credentials.Please try again' })
+        }
+        const token = generateToken({ id: user._id.toString() }, "7d")
+        res.send({
+            id: user._id,
+            username: user.username,
+            picture: user.picture,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            token: token,
+            verified: user.verified,
+            message: "Register Success ! please activate your email to start"
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 }
+
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { home } = require('../controllers/user');
+const { register, activateAccount, login } = require('../controllers/user');
 
 const router = express.Router();
 
 
-router.get("/user", home)
+router.post("/register", register)
+router.post("/activate", activateAccount);
+router.post("/login", login);
 
 module.exports = router;
